2597dd2e8b430c22d494c245cbb2932065fb210f
DanielPierce <danny@pierceplace.net>
2021-03-09 17:15:43 -0600

    Added the GameSceneManager script
    Added a script which controls the initialization of the lander, and has taken over the pause function

--- Assets/Scripts/LanderController.cs
+++ Assets/Scripts/LanderController.cs
@@ -6,32 +6,29 @@ using UnityEngine;
 public class LanderController : MonoBehaviour
 {
 
-    public Rigidbody lander;
-    [Header("Thrust in newtons")]
+    public Rigidbody body;
     public int thrust;
-    [Header("Experienced gravity in m/s/s")]
     public float gravity;
     
-    private bool isPaused = false;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        lander.drag = 0;
+
     }
 
     // Fixed update is called every physics step
     void FixedUpdate()
     {
         // Add affects of gravity
-        lander.AddForce(Vector3.down * gravity, ForceMode.Acceleration);
+        body.AddForce(Vector3.down * gravity, ForceMode.Acceleration);
 
         // If the up arrow is down, apply an impulse this timestep
         if(Input.GetKey(KeyCode.UpArrow))
         {
             // Thrust is newtons, multiply by time since last physics step to get newton-seconds
-            lander.AddForce(Vector3.up * thrust * Time.deltaTime, ForceMode.Impulse);
+            body.AddForce(Vector3.up * thrust * Time.deltaTime, ForceMode.Impulse);
         }
 
 
@@ -40,21 +37,7 @@ public class LanderController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // If the space key was pressed this frame
-        if(Input.GetKeyDown(KeyCode.Space))
-        {
-            // Pause or unpause
-            isPaused = !isPaused;
-            if(isPaused)
-            {
-                Time.timeScale = 0;
-            }
-            else
-            {
-                //Can probably put fudge factor here, set time to like 0.8f instead of 1
-                Time.timeScale = 1;
-            }
-        }
+
     }
 
     void OnCollisionEnter(Collision targetObj)
