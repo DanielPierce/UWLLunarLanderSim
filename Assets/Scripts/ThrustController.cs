0513adb18b2147bd50c39a4d744da9e1357b858b
Logan Larson <loganlarson20@gmail.com>
2021-04-05 21:54:39 -0500

    Init thrust sprite, controller script and added sprites

---
+++ Assets/Scripts/ThrustController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThrustController : MonoBehaviour
+{
+  public bool isThrusting = true;
+
+  // Start is called before the first frame update
+  void Start()
+  {
+
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    if (
+    }
+
+  void ToggleThruster()
+  {
+    if (isThrusting)
+    {
+      isThrusting = false;
+    }
+    else
+    {
+      isThrusting = true;
+    }
+  }
+}
