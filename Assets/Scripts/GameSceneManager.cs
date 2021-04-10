6fa75604cc01d8f4a46fd5615b55a085eddf51ca
Logan Larson <loganlarson20@gmail.com>
2021-04-10 10:56:53 -0500

    Hard Landing pop up

--- Assets/Scripts/GameSceneManager.cs
+++ Assets/Scripts/GameSceneManager.cs
@@ -5,127 +5,135 @@ using UnityEngine.UI;
 
 public class GameSceneManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+  // Start is called before the first frame update
 
-    public LanderController lander;
+  public LanderController lander;
 
-    public Image crashLanding;
-    public Image softLanding;
-    public enum Scenario { Moon, Mars };
-    public enum SimulationMode { Arcade, FullPhys}
+  public Image crashLanding;
+  public Image softLanding;
 
-    public Scenario currentLocation = Scenario.Moon;
-    public SimulationMode simMode = SimulationMode.Arcade;
+  public Image hardLanding;
+  public enum Scenario { Moon, Mars };
+  public enum SimulationMode { Arcade, FullPhys }
 
-    private PhysicsData landerData = null;
+  public Scenario currentLocation = Scenario.Moon;
+  public SimulationMode simMode = SimulationMode.Arcade;
+
+  private PhysicsData landerData = null;
 
 
-    private bool isPaused = false;
+  private bool isPaused = false;
 
-    void Start()
+  void Start()
+  {
+    lander.body.gravityScale = 0;
+    if (currentLocation == Scenario.Moon)
     {
-        lander.body.gravityScale = 0;
-        if(currentLocation == Scenario.Moon)
-        {
-            lander.thrust = 45040; // N
-            lander.gravity = 1.62f; // m/s/s
-            lander.body.drag = 0;
-            lander.body.angularDrag = 0;
-            lander.drymass = 6839; // kg
-            lander.maxFuelMass = 8165; // kg
-            lander.setFuelLevel = lander.maxFuelMass * 0.1f; // Start with 10% fuel remaining
-            lander.currentFuelMass = lander.setFuelLevel;
-            lander.burnRate = 14.75f; // kg/s
-            lander.torque = 150;
-            // Can add additional values here like fuel levels, dry mass, etc
-        }
-        else if(currentLocation == Scenario.Mars)
-        {
-            lander.gravity = 3.72f; // m/s/s
-            // Will have to update these for martian conditions
-        }
-        //others scenarios?
-        if(simMode == SimulationMode.Arcade)
-        {
-            lander.body.angularDrag = 100;
-        }
+      lander.thrust = 45040; // N
+      lander.gravity = 1.62f; // m/s/s
+      lander.body.drag = 0;
+      lander.body.angularDrag = 0;
+      lander.drymass = 6839; // kg
+      lander.maxFuelMass = 8165; // kg
+      lander.setFuelLevel = lander.maxFuelMass * 0.1f; // Start with 10% fuel remaining
+      lander.currentFuelMass = lander.setFuelLevel;
+      lander.burnRate = 14.75f; // kg/s
+      lander.torque = 150;
+      // Can add additional values here like fuel levels, dry mass, etc
     }
-
-    // Update is called once per frame
-    void Update()
+    else if (currentLocation == Scenario.Mars)
     {
-        landerData = lander.GetPhysicsData();
-        HandleLanderThrottle();
-        // If the space key was pressed this frame
-        if (Input.GetKeyDown(KeyCode.P))
-        {
-            // Pause or unpause
-            isPaused = !isPaused;
-            if (isPaused)
-            {
-                Time.timeScale = 0;
-            }
-            else
-            {
-                //Can probably put fudge factor here, set time to like 0.8f instead of 1
-                Time.timeScale = 1;
-                softLanding.enabled = false;
-                crashLanding.enabled = false;
-            }
-        }
-        if(Input.GetKeyDown(KeyCode.Space))
-        {
-            lander.ToggleThruster();
-        }
-        if(Input.GetKeyDown(KeyCode.R))
-        {
-            lander.ResetLander();
-            crashLanding.enabled = false;
-        }
+      lander.gravity = 3.72f; // m/s/s
+                              // Will have to update these for martian conditions
     }
-
-    void OnGUI()
+    //others scenarios?
+    if (simMode == SimulationMode.Arcade)
     {
-        GUI.Label(new Rect(20, 20, 250, 20), "Net force:    " + landerData.netForce);
-        GUI.Label(new Rect(20, 40, 250, 20), "Net torque:   " + landerData.netTorque);
-        GUI.Label(new Rect(20, 60, 250, 20), "Angular vel:  " + landerData.angularVelocity);
-        GUI.Label(new Rect(20, 80, 250, 20), "Angular acc:  " + landerData.angularAcceleration);
-        GUI.Label(new Rect(20, 100, 250, 20), "Velocity:     " + landerData.velocity);
-        GUI.Label(new Rect(20, 120, 250, 20), "Acceleration: " + landerData.acceleration);
-        GUI.Label(new Rect(20, 140, 250, 20), "Altitude:     " + landerData.altitude);
-        GUI.Label(new Rect(20, 160, 250, 20), "Rotation:     " + landerData.degreesRotated);
-        GUI.Label(new Rect(20, 180, 250, 20), "Internal Rot: " + landerData.internalRotation);
-        GUI.Label(new Rect(20, 200, 250, 20), "Thrust force: " + landerData.thrustForce);
-        GUI.Label(new Rect(20, 240, 250, 20), "Current fuel: " + lander.currentFuelMass);
-        GUI.Label(new Rect(20, 260, 250, 20), "Throttle:     " + lander.throttle);
+      lander.body.angularDrag = 100;
     }
+  }
 
-    void FixedUpdate()
+  // Update is called once per frame
+  void Update()
+  {
+    landerData = lander.GetPhysicsData();
+    HandleLanderThrottle();
+    // If the space key was pressed this frame
+    if (Input.GetKeyDown(KeyCode.P))
     {
-        HandleLanderRotation();
+      changePause();
     }
+    if (Input.GetKeyDown(KeyCode.Space))
+    {
+      lander.ToggleThruster();
+    }
+    if (Input.GetKeyDown(KeyCode.R))
+    {
+      lander.ResetLander();
+      crashLanding.enabled = false;
+    }
+  }
 
-    void HandleLanderThrottle()
+  public void changePause()
+  {
+    // Pause or unpause
+    isPaused = !isPaused;
+    if (isPaused)
     {
-        if (Input.GetKey(KeyCode.UpArrow))
-        {
-            lander.IncreaseThrottle();
-        }
-        if (Input.GetKey(KeyCode.DownArrow))
-        {
-            lander.DecreaseThrottle();
-        }
+      Time.timeScale = 0;
     }
+    else
+    {
+      //Can probably put fudge factor here, set time to like 0.8f instead of 1
+      Time.timeScale = 1;
+      softLanding.enabled = false;
+      crashLanding.enabled = false;
+      hardLanding.enabled = false;
+    }
+  }
 
-    void HandleLanderRotation()
+  void OnGUI()
+  {
+    GUI.Label(new Rect(20, 20, 250, 20), "Net force:    " + landerData.netForce);
+    GUI.Label(new Rect(20, 40, 250, 20), "Net torque:   " + landerData.netTorque);
+    GUI.Label(new Rect(20, 60, 250, 20), "Angular vel:  " + landerData.angularVelocity);
+    GUI.Label(new Rect(20, 80, 250, 20), "Angular acc:  " + landerData.angularAcceleration);
+    GUI.Label(new Rect(20, 100, 250, 20), "Velocity:     " + landerData.velocity);
+    GUI.Label(new Rect(20, 120, 250, 20), "Acceleration: " + landerData.acceleration);
+    GUI.Label(new Rect(20, 140, 250, 20), "Altitude:     " + landerData.altitude);
+    GUI.Label(new Rect(20, 160, 250, 20), "Rotation:     " + landerData.degreesRotated);
+    GUI.Label(new Rect(20, 180, 250, 20), "Internal Rot: " + landerData.internalRotation);
+    GUI.Label(new Rect(20, 200, 250, 20), "Thrust force: " + landerData.thrustForce);
+    GUI.Label(new Rect(20, 240, 250, 20), "Current fuel: " + lander.currentFuelMass);
+    GUI.Label(new Rect(20, 260, 250, 20), "Throttle:     " + lander.throttle);
+  }
+
+  void FixedUpdate()
+  {
+    HandleLanderRotation();
+  }
+
+  void HandleLanderThrottle()
+  {
+    if (Input.GetKey(KeyCode.UpArrow))
     {
-        if (Input.GetKey(KeyCode.LeftArrow))
-        {
-            lander.RotateLeft();
-        }
-        if (Input.GetKey(KeyCode.RightArrow))
-        {
-            lander.RotateRight();
-        }
+      lander.IncreaseThrottle();
     }
+    if (Input.GetKey(KeyCode.DownArrow))
+    {
+      lander.DecreaseThrottle();
+    }
+  }
+
+  void HandleLanderRotation()
+  {
+    if (Input.GetKey(KeyCode.LeftArrow))
+    {
+      lander.RotateLeft();
+    }
+    if (Input.GetKey(KeyCode.RightArrow))
+    {
+      lander.RotateRight();
+    }
+  }
 }
--- Assets/Scripts/LanderControllers/LanderController.cs
+++ Assets/Scripts/LanderControllers/LanderController.cs
@@ -5,265 +5,271 @@ using static GameSceneManager;
 public class LanderController : MonoBehaviour
 {
 
-    public Rigidbody2D body;
-    public int thrust;
-    public float gravity;
-    public float torque;
+  public Rigidbody2D body;
+  public int thrust;
+  public float gravity;
+  public float torque;
 
-    public float setFuelLevel;
-    public float maxFuelMass;
-    public float currentFuelMass;
-    public float burnRate;
+  public float setFuelLevel;
+  public float maxFuelMass;
+  public float currentFuelMass;
+  public float burnRate;
 
-    public float throttle;
+  public float throttle;
 
-    public float drymass;
+  public float drymass;
 
-    public Vector2 originalPos;
+  public Vector2 originalPos;
 
-    protected Vector2 previousVelocity;
-    protected float previousAngularVelocity;
+  protected Vector2 previousVelocity;
+  protected float previousAngularVelocity;
 
-    protected PhysicsData record;
+  protected PhysicsData record;
 
-    protected float throttleMax = 1f;
-    protected float throttleMin = 0f;
-    protected float throttleInc = 0.001f;
+  protected float throttleMax = 1f;
+  protected float throttleMin = 0f;
+  protected float throttleInc = 0.001f;
 
-    protected bool thrusterEnabled = true;
+  protected bool thrusterEnabled = true;
 
-    protected const float safeLandingMaxSpeed = 3.0f;
-    protected const float hardLandingMaxSpeed = 6.0f;
+  protected const float safeLandingMaxSpeed = 3.0f;
+  protected const float hardLandingMaxSpeed = 6.0f;
 
-    protected float internalRotation;
+  protected float internalRotation;
 
-    public SpriteRenderer sprite;
-    public Animator animator;
-    
-    public Image crashLanding;
-    public Image softLanding;
+  public SpriteRenderer sprite;
+  public Animator animator;
 
-    // Start is called before the first frame update
-    void Start()
+  public Image crashLanding;
+  public Image softLanding;
+  public Image hardLanding;
+
+  public GameSceneManager gsm;
+
+  // Start is called before the first frame update
+  void Start()
+  {
+    record = new PhysicsData();
+    originalPos = new Vector2(body.position.x, body.position.y);
+    thrusterEnabled = true;
+  }
+
+  // Fixed update is called every physics step
+  public virtual void FixedUpdate()
+  {
+    initializeTimestepVariables();
+
+    if (currentFuelMass <= 0)
     {
-        record = new PhysicsData();
-        originalPos = new Vector2(body.position.x, body.position.y);
-        thrusterEnabled = true;
+      throttle = 0;
+      currentFuelMass = 0;
+      thrusterEnabled = false;
     }
 
-    // Fixed update is called every physics step
-    public virtual void FixedUpdate()
+    if (thrusterEnabled)
     {
-        initializeTimestepVariables();
+      // Transform the rotation to a Vector2
+      Vector2 rotationVector = new Vector2(Mathf.Cos(Mathf.Deg2Rad * internalRotation) * -1, Mathf.Sin(Mathf.Deg2Rad * internalRotation));
 
-        if (currentFuelMass <= 0)
-        {
-            throttle = 0;
-            currentFuelMass = 0;
-            thrusterEnabled = false;
-        }
+      // Apply the thrust using the vector created
+      Vector2 thrustVector = rotationVector * thrust * Time.deltaTime * throttle;
+      body.AddForce(thrustVector, ForceMode2D.Impulse);
 
-        if (thrusterEnabled)
-        {
-            // Transform the rotation to a Vector2
-            Vector2 rotationVector = new Vector2(Mathf.Cos(Mathf.Deg2Rad * internalRotation) * -1, Mathf.Sin(Mathf.Deg2Rad * internalRotation));
+      // Ensure the thrust from the thruster is applied to the net force
+      record.thrustForce = rotationVector * thrust * throttle;
+      record.netForce += record.thrustForce;
 
-            // Apply the thrust using the vector created
-            Vector2 thrustVector = rotationVector * thrust * Time.deltaTime * throttle;
-            body.AddForce(thrustVector, ForceMode2D.Impulse);
+      currentFuelMass -= burnRate * throttle * Time.deltaTime;
+      currentFuelMass = Mathf.Max(currentFuelMass, 0);
+      body.mass = drymass + currentFuelMass;
 
-            // Ensure the thrust from the thruster is applied to the net force
-            record.thrustForce = rotationVector * thrust * throttle;
-            record.netForce += record.thrustForce;
-
-            currentFuelMass -= burnRate * throttle * Time.deltaTime;
-            currentFuelMass = Mathf.Max(currentFuelMass, 0);
-            body.mass = drymass + currentFuelMass;
-
-            animator.SetBool("isThrusting", true); 
-        }
-        else {
-            animator.SetBool("isThrusting", false); 
-        }
-
-        if (throttle == 0) {
-            animator.SetBool("isThrusting", false); 
-        }
-
-        recordPostPhysicsVariables();
-
-        OnPhysicsUpdate();
+      animator.SetBool("isThrusting", true);
+    }
+    else
+    {
+      animator.SetBool("isThrusting", false);
     }
 
-    public virtual void OnPhysicsUpdate()
+    if (throttle == 0)
     {
-        internalRotation = record.degreesRotated;
-        record.internalRotation = internalRotation;
-
-        sprite.transform.rotation = transform.rotation;
-        sprite.transform.position = transform.position;
+      animator.SetBool("isThrusting", false);
     }
 
-    void OnCollisionEnter2D(Collision2D targetObj)
+    recordPostPhysicsVariables();
+
+    OnPhysicsUpdate();
+  }
+
+  public virtual void OnPhysicsUpdate()
+  {
+    internalRotation = record.degreesRotated;
+    record.internalRotation = internalRotation;
+
+    sprite.transform.rotation = transform.rotation;
+    sprite.transform.position = transform.position;
+  }
+
+  void OnCollisionEnter2D(Collision2D targetObj)
+  {
+    // If we handle collisions like this, make sure to correctly assign tags to
+    // the good/bad landing zone objects
+    if (targetObj.gameObject.tag == "FlatTerrain")
     {
-        // If we handle collisions like this, make sure to correctly assign tags to
-        // the good/bad landing zone objects
-        if (targetObj.gameObject.tag == "FlatTerrain")
-        {
-            float velocity = targetObj.relativeVelocity.magnitude;
-            // Check if velocity/orientation are good
-            if (velocity <= safeLandingMaxSpeed)
-            {
-                //soft landing
-                Debug.Log("Soft landing @ speed: " + velocity);
+      float velocity = targetObj.relativeVelocity.magnitude;
+      // Check if velocity/orientation are good
+      if (velocity <= safeLandingMaxSpeed)
+      {
+        //soft landing
+        Debug.Log("Soft landing @ speed: " + velocity);
 
-                //Add UI popup for soft landing here
-                softLanding.enabled = true;
-                //Toggle off thrusters
-            }
-            else if (velocity < hardLandingMaxSpeed)
-            {
-                //Hard Landing
-                Debug.Log("Hard landing @ speed: " + velocity);
+        //Add UI popup for soft landing here
+        softLanding.enabled = true;
+        gsm.changePause();
+        //Toggle off thrusters
+      }
+      else if (velocity < hardLandingMaxSpeed)
+      {
+        //Hard Landing
+        Debug.Log("Hard landing @ speed: " + velocity);
 
-                //Add UI popup for hard landing here
+        //Add UI popup for hard landing here
+        hardLanding.enabled = true;
+        //Add any damages here(20% fuel loss for now)
+        currentFuelMass = currentFuelMass * 0.8f;
 
-                //Add any damages here(20% fuel loss for now)
-                currentFuelMass = currentFuelMass * 0.8f;
+        //Toggle off thrusters
 
-                //Toggle off thrusters
-                
-            }
-            else
-            {
-                Debug.Log("Crashed @ speed: " + velocity);
-                //Crashed
+      }
+      else
+      {
+        Debug.Log("Crashed @ speed: " + velocity);
+        //Crashed
 
-                //Add UI popup for crashing here
-                crashLanding.enabled = true;
-                //Drain remaining fuel (for now) to remove lander's ability to fly
-                throttle = 0;
-                currentFuelMass = 0;
-            }
-
-        }
-
-        if (targetObj.gameObject.tag == "CrashTerrain")
-        {
-            throttle = 0;
-            currentFuelMass = 0;
-            crashLanding.enabled = true; 
-        }
-
-        thrusterEnabled = false;
-        animator.SetBool("isThrusting", false);
-    }
-
-    public PhysicsData GetPhysicsData()
-    {
-        return record;
-    }
-
-    protected virtual void initializeTimestepVariables()
-    {
-        // Add affects of gravity
-        Vector2 gravForce = Vector2.down * gravity * body.mass;
-        body.AddForce(gravForce * Time.deltaTime, ForceMode2D.Impulse);
-
-        // Set initial values for variables to display
-        record.netForce = gravForce;
-        record.netTorque = 0;
-        record.velocity = body.velocity;
-        record.angularVelocity = body.angularVelocity * Mathf.Rad2Deg;
-        record.altitude = body.position.y - (body.transform.localScale.y / 2);
-
-
-        // Calculate the number of degrees from vertical the sprite has rotated
-        record.degreesRotated = -1 * (body.rotation - 90);
-    }
-
-    protected void recordPostPhysicsVariables()
-    {
-
-        // Calculate instantaneous acceleration, and store current velocity for next frame
-        record.acceleration = (body.velocity - previousVelocity) / Time.deltaTime;
-        previousVelocity = body.velocity;
-
-        // Same for instantaneous angular acceleration
-        float angularAccelVector = (body.angularVelocity * Mathf.Rad2Deg - previousAngularVelocity * Mathf.Rad2Deg) / Time.deltaTime;
-        record.angularAcceleration = angularAccelVector;
-        previousAngularVelocity = body.angularVelocity;
+        //Add UI popup for crashing here
+        crashLanding.enabled = true;
+        //Drain remaining fuel (for now) to remove lander's ability to fly
+        throttle = 0;
+        currentFuelMass = 0;
+      }
 
     }
 
-    public void IncreaseThrottle()
+    if (targetObj.gameObject.tag == "CrashTerrain")
     {
-        if (throttle < throttleMax)
-        {
-            throttle += throttleInc;
-        }
-        if (throttle >= throttleMax)
-        {
-            throttle = throttleMax;
-        }
-    }
-    public void DecreaseThrottle()
-    {
-        if (throttle > throttleMin)
-        {
-            throttle -= throttleInc;
-        }
-        if (throttle <= throttleMin)
-        {
-            throttle = throttleMin;
-        }
+      throttle = 0;
+      currentFuelMass = 0;
+      crashLanding.enabled = true;
     }
 
-    public virtual void RotateLeft()
-    {
-        // Rotate counterclockwise in the Z plane here
-        record.netTorque = torque;
-        body.AddTorque(record.netTorque, ForceMode2D.Impulse);
-    }
+    thrusterEnabled = false;
+    animator.SetBool("isThrusting", false);
+  }
 
-    public virtual void RotateRight()
-    {
-        // Rotate clockwise in the Z plane here
-        record.netTorque = torque * -1;
-        body.AddTorque(record.netTorque, ForceMode2D.Impulse);
-    }
+  public PhysicsData GetPhysicsData()
+  {
+    return record;
+  }
 
-    public void ToggleThruster()
-    {
-        thrusterEnabled = !thrusterEnabled;
-    }
+  protected virtual void initializeTimestepVariables()
+  {
+    // Add affects of gravity
+    Vector2 gravForce = Vector2.down * gravity * body.mass;
+    body.AddForce(gravForce * Time.deltaTime, ForceMode2D.Impulse);
 
-    public void ResetLander()
+    // Set initial values for variables to display
+    record.netForce = gravForce;
+    record.netTorque = 0;
+    record.velocity = body.velocity;
+    record.angularVelocity = body.angularVelocity * Mathf.Rad2Deg;
+    record.altitude = body.position.y - (body.transform.localScale.y / 2);
+
+
+    // Calculate the number of degrees from vertical the sprite has rotated
+    record.degreesRotated = -1 * (body.rotation - 90);
+  }
+
+  protected void recordPostPhysicsVariables()
+  {
+
+    // Calculate instantaneous acceleration, and store current velocity for next frame
+    record.acceleration = (body.velocity - previousVelocity) / Time.deltaTime;
+    previousVelocity = body.velocity;
+
+    // Same for instantaneous angular acceleration
+    float angularAccelVector = (body.angularVelocity * Mathf.Rad2Deg - previousAngularVelocity * Mathf.Rad2Deg) / Time.deltaTime;
+    record.angularAcceleration = angularAccelVector;
+    previousAngularVelocity = body.angularVelocity;
+
+  }
+
+  public void IncreaseThrottle()
+  {
+    if (throttle < throttleMax)
     {
-        // Crashed, reset position
-        body.position = originalPos;
-        body.rotation = 0;
-        body.velocity = Vector2.zero;
-        body.angularVelocity = 0;
-        currentFuelMass = setFuelLevel;
+      throttle += throttleInc;
     }
+    if (throttle >= throttleMax)
+    {
+      throttle = throttleMax;
+    }
+  }
+  public void DecreaseThrottle()
+  {
+    if (throttle > throttleMin)
+    {
+      throttle -= throttleInc;
+    }
+    if (throttle <= throttleMin)
+    {
+      throttle = throttleMin;
+    }
+  }
+
+  public virtual void RotateLeft()
+  {
+    // Rotate counterclockwise in the Z plane here
+    record.netTorque = torque;
+    body.AddTorque(record.netTorque, ForceMode2D.Impulse);
+  }
+
+  public virtual void RotateRight()
+  {
+    // Rotate clockwise in the Z plane here
+    record.netTorque = torque * -1;
+    body.AddTorque(record.netTorque, ForceMode2D.Impulse);
+  }
+
+  public void ToggleThruster()
+  {
+    thrusterEnabled = !thrusterEnabled;
+  }
+
+  public void ResetLander()
+  {
+    // Crashed, reset position
+    body.position = originalPos;
+    body.rotation = 0;
+    body.velocity = Vector2.zero;
+    body.angularVelocity = 0;
+    currentFuelMass = setFuelLevel;
+  }
 }
 
 public class PhysicsData
 {
-    public float degreesRotated;
-    public float torque;
+  public float degreesRotated;
+  public float torque;
 
-    public Vector2 velocity;
-    public float angularVelocity;
+  public Vector2 velocity;
+  public float angularVelocity;
 
-    public Vector2 netForce;
-    public Vector2 thrustForce;
-    public float netTorque;
-    public Vector2 acceleration;
-    public float angularAcceleration;
+  public Vector2 netForce;
+  public Vector2 thrustForce;
+  public float netTorque;
+  public Vector2 acceleration;
+  public float angularAcceleration;
 
-    public float altitude;
+  public float altitude;
 
-    public float internalRotation;
+  public float internalRotation;
 }
