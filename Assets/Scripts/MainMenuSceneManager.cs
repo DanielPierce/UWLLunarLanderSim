adbe1fc4f735aa325aa2cdf6d4be1591dc8b0087
Logan <loganlarson20@gmail.com>
2021-05-04 09:58:32 -0500

    Credits

---
+++ Assets/Scripts/MainMenuSceneManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenuSceneManager : MonoBehaviour
+{
+    public Image creditsPopup;
+
+    private bool isCredits = false;
+    
+    public void changeCredits() {
+        isCredits = !isCredits;
+
+        if (isCredits) {
+            creditsPopup.enabled = true;
+        } else {
+            creditsPopup.enabled = false;
+        }
+    }
+}
